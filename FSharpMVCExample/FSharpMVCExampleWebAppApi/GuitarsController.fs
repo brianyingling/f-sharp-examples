f5157046794dcf7854942ce289d22d104cf337d5
Brian Yingling <brian@jet.com>
2014-12-06 18:08:05 -0500

    sample works now

--- FSharpMVCExample/FSharpMVCExampleWebAppApi/GuitarsController.fs
+++ FSharpMVCExample/FSharpMVCExampleWebAppApi/GuitarsController.fs
@@ -2,12 +2,30 @@
 
 open System.Web.Mvc
 open FsWeb.Models
+open FsWeb.Repositories
+
+//[<HandleError>]
+//type GuitarsController() =
+//    inherit Controller()
+//    
+//    member this.Index() =
+//        seq {   yield Guitar(Name = "Gibson Les Paul")
+//                yield Guitar(Name = "Martin D-28")
+//        } |> this.View
 
 [<HandleError>]
-type GuitarsController() =
+type GuitarsController(repository: GuitarsRepository) =
     inherit Controller()
-    
+
+    new() = new GuitarsController(GuitarsRepository())
     member this.Index() =
-        seq {   yield Guitar(Name = "Gibson Les Paul")
-                yield Guitar(Name = "Martin D-28")
-        } |> this.View
+        repository.GetAll()
+        |> this.View
+
+    member this.GetByName name =
+        repository.GetByName name
+        |> this.View
+
+    member this.GetAllAlphabetic() =
+        repository.GetAllAlphabetic()
+        |> this.View
---
+++ FSharpMVCExample/FSharpMVCExampleWebAppApi/GuitarsRepository.fs
@@ -0,0 +1,29 @@
+﻿namespace FsWeb.Repositories
+
+open FsWeb.Models
+
+type GuitarsRepository() =
+    member x.GetAll() =
+        use context = new FsMvcAppEntities()
+        query { for g in context.Guitars do  
+                select g }
+        |> Seq.toList
+
+    member x.GetByName name =
+        use context = new FsMvcAppEntities()
+        query {for g in context.Guitars do
+               where(g.Name = name)}
+        |> Seq.toList
+
+    member x.GetAllAlphabetic() =
+        use context = new FsMvcAppEntities() 
+        query {for g in context.Guitars do
+                sortBy g.Name}
+        |> Seq.toList
+       
+    member x.GetTop rowCount =
+        use context = new FsMvcAppEntities()
+        query {for g in context.Guitars do 
+               take rowCount}
+
+
