dd810c7c7e8a1c503bcf87e8893917c2f878137f
Brian Yingling <brian@jet.com>
2014-12-06 16:17:22 -0500

    init commit

---
+++ FSharpMVCExample/FSharpMVCExampleWebAppApi/GuitarsController.fs
@@ -0,0 +1,13 @@
+﻿namespace FsWeb.Controllers
+
+open System.Web.Mvc
+open FsWeb.Models
+
+[<HandleError>]
+type GuitarsController() =
+    inherit Controller()
+    
+    member this.Index() =
+        seq {   yield Guitar(Name = "Gibson Les Paul")
+                yield Guitar(Name = "Martin D-28")
+        } |> this.View
